0c724915f528bc36ea401268e1d75186f54d10d3
FatalVirus <jeewoong92@hanmail.net>
2017-12-29 20:25:35 +0900

    2017-12-29

--- HelloJava/src/project/v4/SungJukMain.java
+++ HelloJava/src/project/v4/SungJukMain.java
@@ -6,13 +6,15 @@ public class SungJukMain {
 
 	public static void main(String[] args) {
 
-		SungJukDAOimpl sj = new SungJukDAOimpl();
+		SungJukVO sj = new SungJukVO();
+		SungJukDAOimpl sji = new SungJukDAOimpl();
 
 		StringBuffer sb = new StringBuffer();
 		Scanner sc = new Scanner(System.in);
 		sb.append("1. 추가").append("2. 조회").append("3. 상세조회").append("4. 수정").append("5. 삭제").append("0. 종료")
 				.append("입력 >>");
-		String wrongtype = "잘못 입력하셨습니다.";
+		String wrongType = "잘못 입력하셨습니다.";
+		int tsjno = 0;
 
 		while (true) {
 
@@ -22,20 +24,24 @@ public class SungJukMain {
 
 			switch (no) {
 			case 1:
-				sj.insertSungJuk(sj);
+				sji.insertSungJuk(sj);
 				break;
 			case 2:
+				sji.selectSungJuk();
 				break;
 			case 3:
+				sji.selectSungJuk(tsjno);
 				break;
 			case 4:
+				sji.updateSungJuk(sj, tsjno);
 				break;
 			case 5:
+				sji.deleteSUngJuk(tsjno);
 				break;
 			case 0:
 				System.exit(0);
 			}
-			System.out.println(wrongtype);
+			System.out.println(wrongType);
 		}
 	}
 
